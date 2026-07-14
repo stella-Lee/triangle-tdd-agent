c79e750811de81a9ce425708e9ef59c41816b6e5
stella-Lee <41104669+stella-Lee@users.noreply.github.com>
2026-07-14 10:54:29 +0900

    Task 2 Green: reject non-positive sides

--- src/triangle/TriangleClassifier.java
+++ src/triangle/TriangleClassifier.java
@@ -2,6 +2,10 @@ package triangle;
 
 public class TriangleClassifier {
     public TriangleType classify(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return TriangleType.INVALID;
+        }
+
         return TriangleType.EQUILATERAL;
     }
 }
